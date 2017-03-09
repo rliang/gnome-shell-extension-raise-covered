fac16e1bfd26010cebf50e5362c21baa9690c42b
rliang <ricardoliang@gmail.com>
2017-03-09 13:22:59 -0300

    get_tile_match unreliable, use hand-made matching; update screencast

--- extension.js
+++ extension.js
@@ -13,44 +13,63 @@ function disconnect(object) {
   delete object.__handles;
 }
 
-function normal(win) {
+function windowIsNormal(win) {
   return win.window_type === Meta.WindowType.NORMAL;
 }
 
-function place(win, {x, y, width, height}) {
+function windowPlace(win) {
+  let {x, y, width, height} = win.get_work_area_current_monitor();
   width -= 2 * (x += 0.3 * width);
   height -= 2 * (y += 0.2 * height);
   win.move_resize_frame(false, x, y, width, height);
 }
 
-function check(win) {
-  let wins = win.get_workspace().list_windows().filter(normal);
-  let top1 = global.display.sort_windows_by_stacking(wins).pop();
-  let top2 = top1.get_tile_match() || top1;
-  if (!top1.get_maximized() || top1.is_fullscreen())
+function workspaceTopWindows(ws) {
+  let tops = [];
+  let wins = ws.list_windows().filter(windowIsNormal);
+  let t1 = global.display.sort_windows_by_stacking(wins).pop();
+  if (!t1 || t1.is_fullscreen() || !t1.get_maximized())
+    return [tops, wins];
+  tops.push(t1);
+  if (t1.get_maximized() === Meta.MaximizeFlags.BOTH)
+    return [tops, wins.filter(w => tops.indexOf(w) === -1)];
+  let t2 = wins.filter(w => w != t1)
+    .filter(w => w.get_maximized() === Meta.MaximizeFlags.VERTICAL)
+    .filter(w => w.get_frame_rect()
+      .union(t1.get_frame_rect())
+      .contains_rect(t1.get_work_area_current_monitor()))
+    .pop();
+  if (t2)
+    tops.push(t2);
+  return [tops, wins.filter(w => tops.indexOf(w) === -1)];
+}
+
+function workspaceCheck(ws) {
+  let [tops, wins] = workspaceTopWindows(ws);
+  if (!tops.length)
     return;
-  for (let w of wins.filter(w => w != top1 && w != top2)) {
+  for (let w of wins) {
     w.unmaximize(Meta.MaximizeFlags.BOTH);
     w.raise();
-    let r = w.get_frame_rect();
-    if ([top1, top2].map(t => t.get_frame_rect().intersect(r))
-      .some(([b, i]) => i.area() > r.area() * 0.5))
-      place(w, top1.get_work_area_current_monitor());
+    let rect = w.get_frame_rect();
+    let ints = tops.map(t => t.get_frame_rect().intersect(rect)[1]);
+    if (ints.some(int => int.area() > rect.area() * 0.5))
+      windowPlace(w);
   }
 }
 
-function setup(win) {
-  if (!normal(win))
+function windowSetup(win) {
+  if (!windowIsNormal(win))
     return;
-  check(win);
+  workspaceCheck(win.get_workspace());
   for (let s of ['raised', 'size-changed', 'position-changed', 'workspace-changed'])
-    connect(win, s, () => Mainloop.idle_add(() => check(win)));
+    connect(win, s, () => workspaceCheck(win.get_workspace()));
   connect(win, 'unmanaged', () => disconnect(win));
 }
 
 function enable() {
-  global.get_window_actors().map(a => a.meta_window).forEach(setup);
-  connect(global.display, 'window-created', (d, w) => setup(w));
+  global.get_window_actors().map(a => a.meta_window).forEach(windowSetup);
+  connect(global.display, 'window-created', (d, w) => windowSetup(w));
 }
 
 function disable() {
