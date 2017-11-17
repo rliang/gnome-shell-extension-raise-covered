a012b8c0b03a3c99c2b0b5d0c30a3d5c8f3ab23b
rliang <ricardoliang@gmail.com>
2017-11-17 16:21:32 -0200

    Improve logic; Use 3.24 features

--- extension.js
+++ extension.js
@@ -1,78 +1,34 @@
 const Meta = imports.gi.Meta;
-const Mainloop = imports.mainloop;
 
-function connect(object, signal, cb) {
-  if (!object.__handles)
-    object.__handles = [];
-  object.__handles.push(object.connect(signal, cb));
-}
-
-function disconnect(object) {
-  if (object.__handles)
-    object.__handles.forEach(h => object.disconnect(h));
-  delete object.__handles;
-}
-
-function windowIsNormal(win) {
-  return win.window_type === Meta.WindowType.NORMAL;
-}
-
-function windowPlace(win) {
-  let {x, y, width, height} = win.get_work_area_current_monitor();
-  width -= 2 * (x += 0.3 * width);
-  height -= 2 * (y += 0.2 * height);
-  win.move_resize_frame(false, x, y, width, height);
-}
-
-function workspaceTopWindows(ws) {
-  let tops = [];
-  let wins = ws.list_windows().filter(windowIsNormal);
-  let t1 = global.display.sort_windows_by_stacking(wins).pop();
-  if (!t1 || t1.is_fullscreen() || !t1.get_maximized())
-    return [tops, wins];
-  tops.push(t1);
-  if (t1.get_maximized() === Meta.MaximizeFlags.BOTH)
-    return [tops, wins.filter(w => tops.indexOf(w) === -1)];
-  let t2 = wins.filter(w => w != t1)
-    .filter(w => w.get_maximized() === Meta.MaximizeFlags.VERTICAL)
-    .filter(w => w.get_frame_rect()
-      .union(t1.get_frame_rect())
-      .contains_rect(t1.get_work_area_current_monitor()))
-    .pop();
-  if (t2)
-    tops.push(t2);
-  return [tops, wins.filter(w => tops.indexOf(w) === -1)];
-}
-
-function workspaceCheck(ws) {
-  let [tops, wins] = workspaceTopWindows(ws);
-  if (!tops.length)
+function check() {
+  let wins = global.display.sort_windows_by_stacking(global.screen.get_active_workspace().list_windows()).reverse();
+  if (!wins.length || wins.some(w => w.is_fullscreen()))
     return;
+  let rect = wins.shift().get_frame_rect();
   for (let w of wins) {
-    w.unmaximize(Meta.MaximizeFlags.BOTH);
-    w.raise();
-    let rect = w.get_frame_rect();
-    let ints = tops.map(t => t.get_frame_rect().intersect(rect)[1]);
-    if (ints.some(int => int.area() > rect.area() * 0.5))
-      windowPlace(w);
+    let r = w.get_frame_rect();
+    if (rect.contains_rect(r)) {
+      w.unmaximize(Meta.MaximizeFlags.BOTH);
+      if (!rect.equal(r))
+        w.raise();
+    }
+    rect = rect.union(r);
   }
 }
 
-function windowSetup(win) {
-  if (!windowIsNormal(win))
-    return;
-  workspaceCheck(win.get_workspace());
-  for (let s of ['raised', 'size-changed', 'position-changed', 'workspace-changed'])
-    connect(win, s, () => workspaceCheck(win.get_workspace()));
-  connect(win, 'unmanaged', () => disconnect(win));
-}
+let _handles_sc = [];
+let _handles_wm = [];
 
 function enable() {
-  global.get_window_actors().map(a => a.meta_window).forEach(windowSetup);
-  connect(global.display, 'window-created', (d, w) => windowSetup(w));
+  _handles_sc = ['workspace-switched', 'restacked']
+    .map(s => global.screen.connect(s, () => Meta.later_add(0, check)));
+  _handles_wm = ['map', 'size-change', 'size-changed']
+    .map(s => global.window_manager.connect(s, () => Meta.later_add(0, check)));
 }
 
 function disable() {
-  global.get_window_actors().map(a => a.meta_window).forEach(disconnect);
-  disconnect(global.display);
+  for (let h of _handles_sc)
+    global.screen.disconnect(h);
+  for (let h of _handles_wm)
+    global.window_manager.disconnect(h);
 }
